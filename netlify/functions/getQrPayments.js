80ded0c46c76cb32bda9383284a72e67c874b7f2
chrn1506 <chrn1506@gmail.com>
2025-06-11 01:12:34 +0530

    changes related to closeqr

---
+++ netlify/functions/getQrPayments.js
@@ -0,0 +1,55 @@
+const axios = require('axios');
+const Razorpay = require('razorpay');
+const allowedOrigins = [
+  "https://photobooth-zm5n.onrender.com",
+  "http://localhost:4200"
+];
+
+exports.handler = async (event) => {
+  const origin = event.headers.origin;
+  const corsHeaders = {
+    "Access-Control-Allow-Origin": allowedOrigins.includes(origin) ? origin : "*",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Methods": "POST, GET, OPTIONS"
+  };
+
+  if (event.httpMethod === "OPTIONS") {
+    return {
+      statusCode: 200,
+      headers: corsHeaders,
+      body: "Preflight OK"
+    };
+  }
+ const instance = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET
+  });
+  try {
+     if (!event.body) {
+      return {
+        statusCode: 400,
+        headers: corsHeaders,
+        body: JSON.stringify({ error: 'No request body received' }),
+      };
+    }
+    const body = JSON.parse(event.body);
+    const qrId = body.qr_id;
+    const fetchresp=await instance.qrCode.fetchAllPayments(body.qr_id)
+
+
+    return {
+      statusCode: 200,
+      headers: corsHeaders,
+      body: JSON.stringify(fetchresp)
+    };
+
+  } catch (error) {
+    return {
+      statusCode: error.response?.status || 500,
+      headers: corsHeaders,
+      body: JSON.stringify({
+        error: error.response?.data || error.message
+      })
+    };
+  }
+};
